6633ea6eaa7473af9e10f34f6a19428c2db92465
jamenor <96614715+jamenor@users.noreply.github.com>
2021-12-28 20:49:08 +0700

    upload file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# pichetprofile
-Example  upload package to pypi.org
+# (Pichet Profile) Example package upload to pypi.org
+
+PyPi: https://pypi.org/project/pichetprofile/
+
+Hillo guy this package can explain profile and another profile.
+
+### วิธีติดตั้ง
+
+เปิด CMD / Terminal
+
+```python
+pip install pichetprofile
+```
+
+### วิธีใช้งานแพ็คเพจนี้
+
+- เปิด IDLE ขึ้นมาแล้วพิมพ์...
+
+```python
+my = Profile('Pichet')
+my.company = 'agri-product-group'
+my.hobby = ['Youtube','Reading','Sleeping']
+print(my.name)
+my.show_email()
+my.show_myart()
+my.show_hobby()
+```
+
+พัฒนาโดย: Jame nomal
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'pichetprofile',      
+  packages = ['pichetprofile'], 
+  version = '0.0.1',  
+  license='MIT', 
+  description = 'Pichet Profile by Jame normal',
+  long_description=DESCRIPTION,
+  author = 'Jame normal',                 
+  author_email = 'pichet.mt53@gmail.com',     
+  url = 'https://github.com/jamenor/pichetprofile',  
+  download_url = 'https://github.com/jamenor/pichetprofile/archive/v0.0.1.zip',  
+  keywords = ['OOP', 'School', 'jamenor'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Software Development :: Utilities',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
